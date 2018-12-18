25668e7264239dcb769b79f80f8890933e30590a
Kernelgarden <kernelgarden@gmail.com>
2018-12-18 13:56:07 +0900

    trying 1167, I think connection caused problem. tree traverse to bfs??

---
+++ 1167 트리의 지름/main.go
@@ -0,0 +1,132 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"sort"
+)
+
+func main() {
+	var nodeNum int
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Fscanf(reader, "%d\n", &nodeNum)
+
+	nodes := make([]*Node, nodeNum + 1)
+	var root *Node
+
+	//for i := 0; i < nodeNum; i++ {
+	for {
+		var p int
+		fmt.Fscanf(reader, "%d", &p)
+
+		if p == 0 {
+			break
+		}
+
+		if nodes[p] == nil {
+			nodes[p] = MakeNode(p)
+		}
+
+		if root == nil {
+			root = nodes[p]
+		}
+
+		for {
+			var c, w int
+			fmt.Fscanf(reader, "%d %d", &c, &w)
+			if c == -1 {
+				break
+			}
+
+			if nodes[c] == nil {
+				nodes[c] = MakeNode(c)
+			}
+
+			//nodes[p].AddChild(nodes[c], w)
+			nodes[p].Connect(nodes[c], w)
+		}
+	}
+
+	fmt.Println(getLongestPath(root))
+}
+
+type Node struct {
+	val int
+	neigbors []*Node
+	weights []int
+	isVisited bool
+}
+
+func MakeNode(val int) *Node {
+	return &Node{val: val, neigbors: make([]*Node, 0), weights: make([]int, 0), isVisited: false}
+}
+
+func (n1 *Node) Connect(n2 *Node, weight int) {
+	n1.neigbors = append(n1.neigbors, n2)
+	n1.weights = append(n1.weights, weight)
+}
+
+func (parent *Node) AddChild(child *Node, weight int) {
+	/*
+	if _, has := child.children[parent.val]; !has {
+		parent.children[child.val] = child
+		parent.weights[child.val] = weight
+	}
+	*/
+}
+
+func getLongestPath(root *Node) int {
+	longestPath := 0
+
+	lengthToRoot := traverse(root, &longestPath)
+	fmt.Printf("[Debug] lengthToRoot: %v\n", lengthToRoot)
+
+	if lengthToRoot >= longestPath {
+		return lengthToRoot
+	} else {
+		return longestPath
+	}
+}
+
+func bfs(root *Node, longestPath *int) int {
+	
+}
+
+func traverse(root *Node, longestPath *int) int {
+	if root == nil || root.isVisited {
+		return 0
+	}
+
+	root.isVisited = true
+
+	lengths := make([]int, 0)
+	for i := 0; i < len(root.neigbors); i++ {
+		lengths = append(lengths, traverse(root.neigbors[i], longestPath) + root.weights[i])
+	}
+	/*
+	for k, v := range root.children {
+		lengths = append(lengths, traverse(v, longestPath) + root.weights[k])
+	}
+	*/
+	sort.Sort(sort.Reverse(sort.IntSlice(lengths)))
+
+	first := 0
+	second := 0
+
+	if len(lengths) >= 2 {
+		first = lengths[0]
+		second = lengths[1]
+	} else if len(lengths) == 1 {
+		first = lengths[0]
+	}
+
+	totalPath := first + second
+	if totalPath > *longestPath {
+		*longestPath = totalPath
+	}
+
+	fmt.Printf("[Debug] root: %v, path: %v, first: %v, second: %v\n", root.val, totalPath, first, second)
+
+	return first
+}
